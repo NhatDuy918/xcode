4af0fa4b6c99989b625f9a5100514adcd4da1bea
xcode <nhatduy918@gmail.com>
2017-04-08 11:29:22 +0700

    Fist Commit

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var txt_a: UITextField!
+    @IBOutlet var txt_b: UITextField!
+    @IBOutlet var txt_c: UITextField!
+    @IBOutlet var button_giai: UIButton!
+    
+    @IBOutlet var txt_ketqua: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +25,35 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func quadraticSolver(a: Double, b: Double, c: Double) -> String {
+        if abs(a) < 0.00001 {
+            let X = -c/b
+            return "X = \(X)"
+        }
+        let b2_4ac = b*b - (4*a*c)
+        if b2_4ac == 0 {
+            let result = -b/(2*a)
+            return "PT có 1 nghiệm\(result)"
+        }
+        if b2_4ac < 0 {
+            return "Pt Vô nghiệm"
+        }
+        let sq = sqrt(b2_4ac)
+        let x1 = (-b + sq) / (2 * a)
+        let x2 = (-b - sq) / (2 * a)
+        return "X 1 = \(x1)\nX 2 = \(x2)"
+    }
+
+    
+    @IBAction func button_giai_Action(_ sender: Any) {
+        view.endEditing(true)
+        let a : Float = Float(txt_a.text!)!
+        let b : Float = Float(txt_b.text!)!
+        let c : Float = Float(txt_c.text!)!
+        let button_giai = quadraticSolver(a: Double(a), b: Double(b), c: Double(c))
+        txt_ketqua.text = button_giai
+        
     }
 
 
